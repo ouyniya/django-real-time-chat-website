ebca360be2465f702e45e8e6b02daf9429b54b81
niya-s <niya.somkerd@gmail.com>
2025-04-10 13:01:34 +0700

    feat: set up javascript to navigate in the bubble

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,46 @@
 
+let chatName = ''
+let chatSocket = null
+let chatWindowURL = window.location.href
+let chatRoomUuid = Math.random().toString(36).slice(2, 12)
+
+console.log('chatid', chatRoomUuid)
+
+// elements
+
+const chatElement = document.querySelector("#chat");
+const chatJoinElement = document.querySelector("#chat_join");
+const chatRoomElement = document.querySelector("#chat_room");
+const chatIconElement = document.querySelector("#chat_icon");
+const chatOpenElement = document.querySelector("#chat_open");
+const chatWelcomeElement = document.querySelector("#chat_welcome");
+
+// add Event Listener
+
+if (chatOpenElement) {
+  // console.log("Button found!");
+  chatOpenElement.onclick = function (e) {
+    e.preventDefault();
+    // console.log("hello");
+    chatIconElement.classList.add("hidden");
+    chatWelcomeElement.classList.remove("hidden");
+
+    return false;
+  };
+} else {
+  console.log("Button not found.");
+}
+
+if (chatJoinElement) {
+  // console.log("Button found!");
+  chatJoinElement.onclick = function (e) {
+    e.preventDefault();
+    // console.log("hello");
+    chatWelcomeElement.classList.add("hidden");
+    chatRoomElement.classList.remove("hidden");
+
+    return false;
+  };
+} else {
+  console.log("Button not found.");
+}
