346310b36fbacb5e20443748e2280e32e0892a6b
niya-s <niya.somkerd@gmail.com>
2025-04-11 01:06:28 +0700

    feat: created page for lsting the rooms

--- chat/urls.py
+++ chat/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'chat'
+app_name = "chat"
 
 urlpatterns = [
-    path('api/create-room/<str:uuid>/', views.create_room, name='create-room')
+    path("api/create-room/<str:uuid>/", views.create_room, name="create-room"),
+    path("chat-admin/", views.admin, name="admin"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,19 +1,31 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+
+from account.models import User
 
 from .models import Room
 from django.shortcuts import render
 
 
-@csrf_exempt # Only if you’re testing; not recommended in production
+@csrf_exempt  # Only if you’re testing; not recommended in production
 @require_POST
 def create_room(request, uuid):
-    name = request.POST.get('name', '')
-    url = request.POST.get('url', '')
+    name = request.POST.get("name", "")
+    url = request.POST.get("url", "")
 
     Room.objects.create(uuid=uuid, client=name, url=url)
 
-    return JsonResponse({ 'message': 'room created' })
+    return JsonResponse({"message": "room created"})
+
+
+@login_required
+def admin(request):
+    rooms = Room.objects.all()
+    users = User.objects.filter(is_staff=True)
+
+    return render(request, "chat/admin.html", {"rooms": rooms, "users": users})
