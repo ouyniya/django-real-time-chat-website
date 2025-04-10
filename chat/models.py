7026fe75f33802924b7e9f7c5542e7c7446e6c8c
niya-s <niya.somkerd@gmail.com>
2025-04-10 11:07:14 +0700

    feat: set up models for message

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+
+class Message(models.Model):
+    body = models.TextField()
+    sent_by = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL
+    )
+
+    class Meta:
+        ordering: "created_at"
+
+    def __str__(self):
+        return f"{self.sent_by}"
