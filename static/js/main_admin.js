7f97a731d53b3a230b11b5f56d7e0423e188ef69
niya-s <niya.somkerd@gmail.com>
2025-04-11 11:24:01 +0700

    feat: recieve and show real-time messages

--- static/js/main_admin.js
+++ static/js/main_admin.js
@@ -1,2 +1,67 @@
+// variables
 
-console.log('main admin')
+const chatRoom = JSON.parse(document.getElementById("room_uuid").textContent);
+
+let chatSocket = null;
+
+console.log(chatRoom);
+
+/* elements */
+
+const chatLogElement = document.querySelector("#chat_log");
+const chatInputElement = document.querySelector("#chat_message_input");
+const chatSubmitElement = document.querySelector("#chat_message_submit");
+
+/** function */
+
+function onChatMessage(data) {
+  console.log("onChatMessage", data);
+
+  if (data.type === "chat_message") {
+    // console.log("agent***", data.agent);
+    if (!data.agent) {
+      chatLogElement.innerHTML += `<div class="flex w-full mt-2 space-x-3 max-w-md">
+            <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">
+              ${data.initials}
+            </div>
+            <div>  
+              <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                <p class="text-sm">${data.message}</p>
+              </div>
+              <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+            </div>
+          </div>`;
+    } else {
+      chatLogElement.innerHTML += `<div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+            <div>  
+              <div class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
+                <p class="text-sm">${data.message}</p>
+              </div>
+              <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+            </div>
+  
+              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">
+              ${data.initials}
+            </div>
+          </div>`;
+    }
+  }
+}
+
+/* web socket */
+
+chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoom}/`);
+
+chatSocket.onmessage = function (e) {
+  console.log("onmessage");
+
+  onChatMessage(JSON.parse(e.data));
+};
+
+chatSocket.onopen = function (e) {
+  console.log("onopen");
+};
+
+chatSocket.onclose = function (e) {
+  console.log("chat socket closed unexpectedly");
+};
